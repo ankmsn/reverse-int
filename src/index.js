e2ab9fd876a455da31a37700e6b2174e26a06848
ankmsn <khomstyle@gmail.com>
2023-01-05 17:43:18 +0000

    init: made test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  let strNum = n.toString();
+  if (strNum[0] === '-') {strNum = strNum.slice(1)}
+  for (i = strNum.length-1; i >= 0; i--) {
+       result += strNum[i];
+  }
+return +result;
+
 }
+
+// console.log(module.exports(-12340));
